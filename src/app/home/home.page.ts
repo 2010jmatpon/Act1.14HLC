d6b14f7453061ce9953b6c1cab8c3500b99b3b21
2010jmatpon <jmatpon2010@g.educaand.es>
2023-11-09 12:41:28 +0100

    Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  rappers: any;
+  filtro: string = "";
+  constructor(private httpClient:HttpClient) {
+    this.rappers = this.httpClient.get('https://raw.githubusercontent.com/2010jmatpon/Act1.14HLC/master/MOCK_DATA.json');
+  }
 
 }
